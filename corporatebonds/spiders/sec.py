49259d43d86be50fd2f87ac00eb82ddaecad08ef
Brett Casbeer <casbeebc@gmail.com>
2016-12-31 18:23:26 -0500

    Added ability to search multiple pages for prospectus documents

--- corporatebonds/spiders/sec.py
+++ corporatebonds/spiders/sec.py
@@ -10,12 +10,26 @@ from corporatebonds.items import BondItem
 class SecSpider(CrawlSpider):
     name = "sec"
     allowed_domains = ["sec.gov"]
+    initial_item_count = 100
+    url_template = None
     
     def __init__(self, symbol=None, *args, **kwargs):
         super(SecSpider, self).__init__(*args, **kwargs)
-        self.start_urls = ['https://www.sec.gov/cgi-bin/browse-edgar?CIK=%s&type=424B2&dateb=&owner=exclude&count=100' % symbol]
-    
+        self.url_template = 'https://www.sec.gov/cgi-bin/browse-edgar?CIK=%s&type=424B2&dateb=&owner=exclude&count=100' % symbol
+        self.start_urls = [self.url_template]
+        self.initial_item_count = 100
+        self.current_item_count = 0
+        
     def parse(self, response):
+        self.parse_sec_page(response)
+        #https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK=0000029915&type=&dateb=&owner=exclude&start=1300&count=100
+        for page in response.xpath("//div[@id='contentDiv']/input[@value='Next 100']").extract():
+            self.logger.info("BLAH!!!")
+            self.current_item_count += self.initial_item_count
+            self.url_template.append("&start=%s" % self.current_item_count)
+            yield Request(response.urljoin(url), callback=self.parse)
+        
+    def parse_sec_page(self, response):
         foundProspectus = False
         
         for searchText in response.xpath("//*[contains(text(),'Ratio of Earnings')]"):
@@ -30,7 +44,7 @@ class SecSpider(CrawlSpider):
             
         for url in response.xpath("//table[@summary='Document Format Files']/tr/td/a/@href").extract():
             if re.compile(r".*\.htm").match(url) is not None:
-                yield Request(response.urljoin(url), callback=self.parse)
+                yield Request(response.urljoin(url), callback=self.parse_sec_page)
           
         for url in response.xpath("//table[@summary='Results']/tr/td[2]/a/@href").extract():
-            yield Request(response.urljoin(url), callback=self.parse)
+            yield Request(response.urljoin(url), callback=self.parse_sec_page)
